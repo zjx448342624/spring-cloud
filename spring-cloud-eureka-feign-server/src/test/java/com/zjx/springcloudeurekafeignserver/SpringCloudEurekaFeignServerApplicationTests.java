de6b020e81ddc39565264441b55d29a6167f267d
zjx <448342624@qq.com>
2020-02-24 00:44:42 +0800

    spring-cloud-eureka

---
+++ spring-cloud-eureka-feign-server/src/test/java/com/zjx/springcloudeurekafeignserver/SpringCloudEurekaFeignServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjx.springcloudeurekafeignserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaFeignServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
